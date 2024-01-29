4771d79bc3015330a50d8f85d23b0947f7beb5e4
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2024-01-29 14:28:17 -0500

    Completed Course class

--- app/src/main/java/com/example/a2340project/Assignment.java
+++ app/src/main/java/com/example/a2340project/Assignment.java
@@ -1,2 +1,35 @@
-package com.example.a2340project;public class Assignment {
+package com.example.a2340project;
+
+import java.util.Date;
+
+public class Assignment {
+    private Date dueDate;
+    private String detail;
+    private String title;
+
+    public Assignment(Date dueDate, String detail, String title) {
+        this.dueDate = dueDate;
+        this.detail = detail;
+        this.title = title;
+    }
+
+    public Assignment(Date dueDate, String title) {
+        this(dueDate, title, "");
+    }
+
+    public Assignment(Date dueDate) {
+        this(dueDate, "");
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
