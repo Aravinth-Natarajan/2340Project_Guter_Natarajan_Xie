ba5820be00013c09155ba7b554d0fab9b4c73a21
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2024-02-08 03:11:19 -0500

    Update TaskClassComparator.java

--- app/src/main/java/com/example/a2340project/TaskClassComparator.java
+++ app/src/main/java/com/example/a2340project/TaskClassComparator.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 public class TaskClassComparator implements Comparator<Task>  {
     @Override
     public int compare(Task o1, Task o2) {
-        int completionComp = Boolean.compare(o1.getChecked(), o2.getChecked());
         int classComp;
 
         if (o1.getCourse() == null && o2.getCourse() != null) {
@@ -18,6 +17,6 @@ public class TaskClassComparator implements Comparator<Task>  {
             classComp = o1.getCourse().compareTo(o2.getCourse());
         }
 
-        return completionComp != 0 ? completionComp : classComp;
+        return classComp;
     }
 }
